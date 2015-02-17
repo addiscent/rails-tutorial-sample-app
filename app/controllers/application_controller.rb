783e5733c68f41680a3a50f0a15ec171c89ea456
Charles Thomaston <ckthomaston@dalorweb.com>
2015-02-17 10:20:51 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-    render text: "Rex says, the Rails Tutorial Sample App will be developing here soon..."
-  end
+#   def hello
+#    render text: "Rex says, the Rails Tutorial Sample App will be developing here soon..."
+#   end
 end
